62843e12167098c8402c05a63c64d8c7fe6dd72f
Aprendiz-00 <chatgptplus26@hotmail.com>
2026-08-13 22:37:41 -0300

    foundation: add modern build identity header

---
+++ Source/Modern/Foundation/BuildInfo.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string_view>
+
+namespace wyd::modern {
+
+inline constexpr std::string_view kModernizationTrack = "foundation";
+inline constexpr int kArchitectureRevision = 1;
+
+} // namespace wyd::modern
